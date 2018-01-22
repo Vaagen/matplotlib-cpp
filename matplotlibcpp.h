7b0c28f5e2e0931fbaa62c936875badfdafc3da9
Vaagen <pcthrane@stud.ntnu.no>
2018-01-22 19:45:33 +0100

    Adding whole path to numpy headers for my setup.

--- matplotlibcpp.h
+++ matplotlibcpp.h
@@ -16,7 +16,7 @@
 
 #ifndef WITHOUT_NUMPY
 #  define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#  include <numpy/arrayobject.h>
+#  include </usr/local/lib/python2.7/site-packages/numpy/core/include/numpy/arrayobject.h>
 #endif // WITHOUT_NUMPY
 
 #if PY_MAJOR_VERSION >= 3
